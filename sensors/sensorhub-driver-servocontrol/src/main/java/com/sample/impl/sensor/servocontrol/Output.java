734eb5a46969988ea735b4db8470adf1fb84419a
MichaelElmore1 <135364586+MichaelElmore1@users.noreply.github.com>
2023-06-05 11:32:05 -0500

    Rename driver

--- sensors/sensorhub-driver-servocontrol/src/main/java/com/sample/impl/sensor/servocontrol/Output.java
+++ sensors/sensorhub-driver-servocontrol/src/main/java/com/sample/impl/sensor/servocontrol/Output.java
@@ -11,7 +11,7 @@
  Copyright (C) 2020-2021 Botts Innovative Research, Inc. All Rights Reserved.
 
  ******************************* END LICENSE BLOCK ***************************/
-package com.sample.impl.sensor.drivername;
+package com.sample.impl.sensor.servocontrol;
 
 import net.opengis.swe.v20.*;
 import org.sensorhub.api.data.DataEvent;
